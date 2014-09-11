5d1af4ef4660424faf9b35499b0faa263f35c61c
rntmdgus <rntmdgus@naver.com>
2014-09-11 16:05:54 +0900

    9
    9

--- sketch.js
+++ sketch.js
@@ -1,25 +1,11 @@
+
 function setup() {
   createCanvas(640, 480);
-  background(128, 0, 64);
   noFill();
-
   stroke(255, 0, 0);
-
-/*
-  var x;
-
-  x = 1;
-  ellipse(x * 150, 100, 50, 50);
-
-  x = 2;
-  ellipse(x * 150, 100, 50, 50);
-
-  x = 3;
-  ellipse(x * 150, 100, 50, 50);
-*/
-
-// for (시작상태 : 반복하는 조건; 증가값)
-
+}
+function draw() {
+  background(128, 0, 64);
 	for (var x = 1; x <=8; x++)
 		// 1~8까지 증가한다(++)
 	{
@@ -28,4 +14,6 @@ function setup() {
 		// y축을 25씩 차이나지만 시작부분이 짤리므로 +100
 		// 원의 가로, 세로의값을 x값의 곱하기 10만큼씩 증가.
 	}
+  var size = Math.abs(mouseX - 320);
+  ellipse(320, 240, mouseX, mouseY);
 }
